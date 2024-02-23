81ad6d1931a46c461446b2f41ebbe67786b48af8
Code A Program <iamsmartsri@gmail.com>
2024-02-23 22:18:34 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+import "./style.css";
+
+const ELE = document.documentElement;
+const mobileNav = document.getElementById("mobile-nav");
+const mobileNavItem = document.querySelectorAll("#mobile-nav li");
+const backDrop = document.getElementById("backdrop");
+const menuBar = document.querySelector("#menubar");
+const menuBarIcon = document.querySelector("#menubar i");
+
+document.querySelectorAll(".theme-switch").forEach((item) =>
+  item.addEventListener("click", () => {
+    ELE.classList.toggle("dark");
+    ELE.classList.contains("dark")
+      ? localStorage.setItem("theme", "dark")
+      : localStorage.setItem("theme", "light");
+  })
+);
+
+const options = [backDrop, menuBar, ...mobileNavItem];
+
+options.forEach((item) =>
+  item.addEventListener("click", () => {
+    mobileNav.classList.toggle("h-0");
+    mobileNav.classList.toggle("h-96");
+    menuBarIcon.classList.toggle("fa-xmark");
+    menuBarIcon.classList.toggle("fa-bars");
+    backDrop.classList.toggle("hidden");
+    document.documentElement.classList.toggle("overflow-hidden");
+  })
+);
+
+ELE.classList.add(localStorage.getItem("theme"));
