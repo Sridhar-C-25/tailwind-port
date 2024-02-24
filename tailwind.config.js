c88dfc90cf9f8a40b925bfb1c4c1e7808e3a8655
Code A Program <iamsmartsri@gmail.com>
2024-02-24 17:52:58 +0530

    corrections

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ export default {
       container: {
         center: true,
       },
-
+      fontFamily: {
+        Oswald: ["Oswald", "sans serif"],
+        Inter: ["Inter"],
+        Poppins: ["Poppins"],
+      },
+      backgroundImage: {
+        heroLight:
+          "url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')",
+        heroDark:
+          "url('https://preline.co/assets/svg/examples-dark/polygon-bg-element.svg')",
+      },
       colors: {
         primary: "#b033fd",
       },
